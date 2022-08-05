b583d4e9d5dad8b3046aebc860d7abdec94f0837
Liesl White <lieslfwhite@gmail.com>
2022-08-05 16:29:26 -0700

    class notes for binary search complete but not all tests passing

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,4 +1,4 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
@@ -6,10 +6,50 @@ class BinarySearchTree(BinaryTree):
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    # def __init__(self):
+    #     # initialization here
+    #     pass
 
-    def some_method(self):
-        # method body here
-        pass
+    def add(self, value):
+        """
+        wrap value in a Node and add it at correct spot
+        """
+        node = Node(value)
+        if not self.root:
+            self.root = node
+
+        def track(root, add_node):
+            if root is None:
+                return
+
+            if add_node.value < root.value:
+                if root.left is None:
+                    # spot available
+                    root.left = add_node
+                else:
+                    track(root.left, add_node)
+            else:
+                if root.right is None:
+                    # spot available
+                    root.right = add_node
+                else:
+                    track(root.right, add_node)
+
+        track(self.root, node)
+
+    def contains(self, target):
+
+        def track(root):
+            if root is None:
+                return False
+
+            if target == root.value:
+                return True
+
+            if target < root.value:
+                return track(root.left)
+            else:
+                return track(root.right)
+
+        return track(self.root)
+
