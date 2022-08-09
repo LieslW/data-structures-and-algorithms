c826426b6589076bd437556eb22026c399710525
Liesl White <lieslfwhite@gmail.com>
2022-08-08 21:03:59 -0700

    tests passing for challenge16

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -3,24 +3,20 @@ from data_structures.queue import Queue
 
 
 def breadth_first(tree):
+    breadth = Queue()
+    nodes = []
 
-    queue_filled = []
-    queue = Queue()
-
-    if tree is None:
-        return queue_filled
-
-    if tree.root:
-        queue.enqueue(tree.root)
-
-    while queue.front:
-        item = queue.dequeue()
-        queue_filled.append(item.value)
-
-        if item.left:
-            queue.enqueue(item.left)
-
-        if item.right:
-            queue.enqueue(item.right)
-
-    return queue_filled
+    if not tree:
+        return nodes
+    if not tree.root:
+        return nodes
+    if not breadth.front:
+        breadth.enqueue(tree.root)
+    while breadth.front:
+        root = breadth.dequeue()
+        nodes.append(root.value)
+        if root.left:
+            breadth.enqueue(root.left)
+        if root.right:
+            breadth.enqueue(root.right)
+    return nodes
