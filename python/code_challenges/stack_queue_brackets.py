4e45a7900c6c401994cd0ba8ec2234c91cfb5491
Liesl White <lieslfwhite@gmail.com>
2022-08-08 23:08:43 -0700

    code challenge 13 complete, tests pasing

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,48 @@
-from data_structures.queue import Queue
+from data_structures.stack import Stack
 
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(input_):
+    print(f"input is {input_}")
+    stack = Stack()
+
+    for i in input_:
+        print(f"checking {i}")
+        if i == "{":
+            stack.push("{")
+        if i == "(":
+            stack.push("(")
+        if i == "[":
+            stack.push("[")
+            print(f"{stack.top.value} is the value on top of the stack")
+
+        if i == "}":
+            try:
+                if stack.top.value == "{":
+                    stack.pop()
+                else:
+                    return False
+            except:
+                return False
+
+        if i == ")":
+            try:
+                if stack.top.value == "(":
+                    stack.pop()
+                else:
+                    return False
+            except:
+                return False
+
+        if i == "]":
+            try:
+                if stack.top.value == "[":
+                    stack.pop()
+                else:
+                    return False
+            except:
+                return False
+
+    if stack.is_empty() == True:
+        return True
+    else:
+        return False
