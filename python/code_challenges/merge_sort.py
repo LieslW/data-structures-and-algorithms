efa298a4a869403c7b1b00252c039b8693e05b51
Liesl White <lieslfwhite@gmail.com>
2022-08-25 15:45:21 -0700

    code and basic test populated merge_sort

--- python/code_challenges/merge_sort.py
+++ python/code_challenges/merge_sort.py
@@ -1,35 +1,34 @@
+def merge_sort(list):
+    n = len(list)
 
+    if n > 1:
+        mid = n // 2
+        left = list[:mid]
+        right = list[mid:]
 
-# ALGORITHM Mergesort(arr)
-#     DECLARE n <-- arr.length
-#
-#     if n > 1
-#       DECLARE mid <-- n/2
-#       DECLARE left <-- arr[0...mid]
-#       DECLARE right <-- arr[mid...n]
-#       // sort the left side
-#       Mergesort(left)
-#       // sort the right side
-#       Mergesort(right)
-#       // merge the sorted left and right sides together
-#       Merge(left, right, arr)
-#
-# ALGORITHM Merge(left, right, arr)
-#     DECLARE i <-- 0
-#     DECLARE j <-- 0
-#     DECLARE k <-- 0
-#
-#     while i < left.length && j < right.length
-#         if left[i] <= right[j]
-#             arr[k] <-- left[i]
-#             i <-- i + 1
-#         else
-#             arr[k] <-- right[j]
-#             j <-- j + 1
-#
-#         k <-- k + 1
-#
-#     if i = left.length
-#        set remaining entries in arr to remaining values in right
-#     else
-#        set remaining entries in arr to remaining values in left
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                list[k] = left[i]
+                i += 1
+            else:
+                list[k] = right[j]
+                j += 1
+            k += 1
+
+        if i == len(left):
+            while j < len(right):
+                list[k] = right[j]
+                k += 1
+                j += 1
+        else:
+            while i < len(left):
+                list[k] = left[i]
+                k += 1
+                i += 1
+
+    return list
--- python/tests/code_challenges/test_merge_sort.py
+++ python/tests/code_challenges/test_merge_sort.py
@@ -1,2 +1,7 @@
 import pytest
+from code_challenges.merge_sort import merge_sort
+
+
+def merge_sort_exists():
+    assert merge_sort
 
